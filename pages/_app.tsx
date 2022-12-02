31bfc55b384dd79d3e602276e8678330c38137c5
ynccsyd <muhibbi_10_@hotmaile.com>
2022-12-02 20:06:11 +0300

    Added Project

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+import type { AppProps } from "next/app"
+import { ChakraProvider } from "@chakra-ui/react"
+
+import { extendTheme } from "@chakra-ui/react"
+
+const colors = {
+  background: "#1F1F1F",
+  accent: "#833BBE",
+  bodyText: "rgba(255, 255, 255, 0.75)",
+}
+
+const theme = extendTheme({ colors })
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,48 @@
+import { Box, Center, Spacer, Stack } from "@chakra-ui/react"
+import type { NextPage } from "next"
+import Head from "next/head"
+import styles from "../styles/Home.module.css"
+import NavBar from "../components/NavBar"
+const Home: NextPage = () => {
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Buildoors</title>
+        <meta name="The NFT Collection for Buildoors" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Box
+        w="full"
+        h="calc(100vh)"
+        bgImage={"url(/home-background.svg)"}
+        backgroundPosition="center"
+      >
+        <Stack w="full" h="calc(100vh)" justify="center">
+					{ /* NavBar */ }
+
+          <Spacer />
+          <Center>
+						{ /* If connected, the second view, otherwise the first */ }
+                </Center>
+          <Spacer />
+
+          <Center>
+            <Box marginBottom={4} color="white">
+              <a
+                href="https://twitter.com/_buildspace"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                built with @_buildspace
+              </a>
+            </Box>
+          </Center>
+        </Stack>
+      </Box>
+    </div>
+  )
+}
+
+export default Home
